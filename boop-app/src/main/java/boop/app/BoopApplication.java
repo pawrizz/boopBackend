dec5f60bb00ff19db38c92600780b7ff69d74da9
Abhishek Singh <abhisheksingh1395@gmail.com>
2026-01-19 19:50:02 +0530

    hibernate

--- boop-app/src/main/java/boop/app/BoopApplication.java
+++ boop-app/src/main/java/boop/app/BoopApplication.java
@@ -2,8 +2,10 @@ package boop.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan(basePackages = "boop")
 public class BoopApplication {
     public static void main(String[] args) {
         SpringApplication.run(BoopApplication.class, args);
